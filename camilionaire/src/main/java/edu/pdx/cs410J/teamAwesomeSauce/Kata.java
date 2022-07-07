7b399fa1db46d5b3a14d6c84f7573ba154487e56
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-07-06 20:44:56 -0700

    we worked on it a bit as a group

--- camilionaire/src/main/java/edu/pdx/cs410J/teamAwesomeSauce/Kata.java
+++ camilionaire/src/main/java/edu/pdx/cs410J/teamAwesomeSauce/Kata.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.teamAwesomeSauce;
 
+
 /**
  * A class for getting started with a code kata
  *
@@ -8,7 +9,37 @@ package edu.pdx.cs410J.teamAwesomeSauce;
  */
 public class Kata {
 
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+  public static String compute(String aString) {
+    int pNum = Integer.parseInt(aString);
+    String startString = "";
+//    StringBuilder result = new StringBuilder();
+    if (pNum % 3 == 0) {
+//      System.out.println("Foo");
+//      startString += "Foo";
+      startString = startString.concat("Foo");
+//      result.append("Foo");
+    }
+    if (pNum % 5 == 0) {
+//      startString += "Bar";
+//      result.append("Bar");
+      startString = startString.concat("Bar");
+    }
+//    if (result.toString().equals("")) {
+    if (! startString.equals("")) {
+//      return result.toString();
+      return startString;
+    } else {
+      return aString;
+    }
   }
+
+  public static void main(String[] args) {
+
+    System.out.println(compute("1"));
+    System.out.println(compute("3"));
+    System.err.println("Missing command line arguments");
+
+
+
+    }
 }
--- camilionaire/src/test/java/edu/pdx/cs410J/teamAwesomeSauce/KataTest.java
+++ camilionaire/src/test/java/edu/pdx/cs410J/teamAwesomeSauce/KataTest.java
@@ -2,12 +2,31 @@ package edu.pdx.cs410J.teamAwesomeSauce;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 public class KataTest
 {
 
   @Test
-  void canInstantiateKataClass() {
-    new Kata();
+  void ifNumberIsOneWrite1() {
+    Kata aKata = new Kata();
+//    System.out.println(aKata.compute("1"));
+//    assertEquals(aKata.compute("1"), "1");
+    assertThat(aKata.compute("1"),   containsString("1"));
+  }
+
+  @Test
+  void ifNumberIsDivThreeWriteFoo() {
+    Kata aKata = new Kata();
+
+    assertThat(aKata.compute("3"), containsString("Foo"));
+  }
+
+  @Test
+  void ifNumberIsDivFiveAddBar() {
+    Kata aKata = new Kata();
+    assertTrue(aKata.compute("5").equals("Bar"));
   }
 
 }
